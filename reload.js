d2740c316ba01951856b53f531f7cb112f74f0a8
deanturpin <git@lvbtn.uk>
2016-12-12 11:24:19 +0000

    remove commented code

--- reload.js
+++ reload.js
@@ -1,9 +1,6 @@
-
 onload = function() {
 
 	"use strict"
-
-	console.log(new Date())
 
 	// Create form
 	var form = document.createElement("form")
@@ -15,29 +12,17 @@ onload = function() {
 	input.setAttribute("type", "checkbox")
 	input.setAttribute("checked", true)
 	input.setAttribute("id", "reload")
-	input.htmlFor = "reload"
-	input.setAttribute("value", "reload")
-	// input.text
-	// input.setProperty("checked", false)
 	input.name = "reload"
-	// input.setProperty("checked", true)
-
-	// <form style="float: right">
-	// <input type="checkbox" id="reload" checked>reload<br>
-	// </form>
 
 	// Add it to the DOM
 	form.appendChild(input)
 	body.appendChild(form)
-	// document.head.appendChild(form)
 
 	// Periodically check reload checkbox state
 	setInterval(function () {
 
 		if (document.getElementById("reload").checked === true) {
-
 			window.location.reload()
 		}
-
-	}, 1000)
+	}, 2000)
 }
